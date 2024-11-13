f6f68f6c79065b8829130e12f309bdf964bc41f9
DucLUT <minhduc16032004@gmail.com>
2024-11-14 00:36:17 +0200

    add new stuff

---
+++ Insertion.java
@@ -0,0 +1,31 @@
+public class Insertion {
+    public static void sort(Comparable[] a) {
+        for (int i = 1; i < a.length; i++){
+            for (int j = i; j > 0 && less(a[j], a[j-1]); j--){
+                exch(a, j, j-1);
+
+            }
+        }
+    }
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Integer[] a = { 3, 4, 33, 432, 34, 3333 };
+        sort(a);
+        show(a);
+    }
+}
---
+++ Selection.java
@@ -0,0 +1,37 @@
+public class Selection {
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        String[] a = { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E" };
+        sort(a);
+        show(a);
+    }
+}
